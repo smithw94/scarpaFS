9ea1635024b91800ed64e7f36e169385cabcc1bb
smithw94 <smithw1994@gmail.com>
2019-11-05 14:26:30 -0800

    background image imported

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => (
-
-  <div>
-    <Link to="/products">All products</Link>
+  
+  <div className="homePage1">
+    <div>
+      <span>SCARPA</span>
+      <h1>ROCK CLIMBING</h1>
+    </div>
+    <div className="product-nav">
+      <Link to="/products">Shop all</Link>
+      <Link to="/products">Rock Shoes</Link>
+      <Link to="/products">Approach</Link>
+    </div>
   </div>
+    
+  
 );
+  
 
 export default Home;
--- frontend/components/home/home_container.jsx
+++ frontend/components/home/home_container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+
 import Home from './home';
 
 const mSTP = state => ({
