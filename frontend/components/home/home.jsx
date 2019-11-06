660e8e9c6b13d0f237b5f8f46f50faf0a41f026a
smithw94 <smithw1994@gmail.com>
2019-11-05 22:14:36 -0800

    home image centered

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 const Home = () => (
-  
-  <div className="homePage1">
-    <div>
-      <span>SCARPA</span>
-      <h1>ROCK CLIMBING</h1>
-    </div>
-    <div className="product-nav">
-      <Link to="/products">Shop all</Link>
-      <Link to="/products">Rock Shoes</Link>
-      <Link to="/products">Approach</Link>
+  <div className="homePage">
+    <div className="homePage1">
+      <div className="homePage1b">
+        <span>SCARPA</span>
+        <h1>ROCK CLIMBING</h1>
+      </div>
+      <div className="product-nav">
+        <Link to="/products">Shop all</Link>
+        <Link to="/products">Rock Shoes</Link>
+        <Link to="/products">Approach</Link>
+      </div>
     </div>
   </div>
+ 
     
   
 );
