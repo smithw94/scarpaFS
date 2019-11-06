c29bdfae2d41eae8968210476e00bc0fd0c04b12
smithw94 <smithw1994@gmail.com>
2019-11-06 13:32:06 -0800

    added home logo

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+// import Logo from 'https://i.imgur.com/liak4zT.png';
 
 
 
 const Home = () => (
   <div className="homePage">
     <div className="homePage1">
+      <img className="homePhoto1" src={"https://i.imgur.com/liak4zT.png"} alt="asdfasdf"/>
       <div className="homePage1b">
         <span>SCARPA</span>
         <h1>ROCK CLIMBING</h1>
