41d98d61199c5ee6f12910cd86cd195d0fa8cb64
smithw94 <smithw1994@gmail.com>
2019-11-08 15:25:24 -0800

    Styled product shoe page

--- frontend/components/products/product_show.jsx
+++ frontend/components/products/product_show.jsx
@@ -13,23 +13,59 @@ class ProductShow extends React.Component {
   render() {
     let { product, prodAttrs } = this.props;
     return(
-      <div>
-        {product.name}
+      <div className="product-show-page">
         
-        <div>
-          <img className="showImage" src={"https://i.imgur.com/5AQShJk.png"} />
+        <div className="product-show">
+          
+          <div className="product-show-image">
+            <img className="showImage" src={"https://i.imgur.com/5AQShJk.png"} />
+          </div>
+          
+          <div className="product-show-info">
+            
+            <div className="product-show-name">
+              <h3>{product.name}</h3>
+            </div>
+            <div className="product-show-price">
+              <p>${product.price}0 USD</p>
+            </div>
+
+            <div className="product-rating">
+              <h3>*****</h3>
+              <p>Reviews Add Your Review</p>
+            </div>
+            
+            <div className="product-show-description">
+                {product.description}
+            </div>
+
+            <div className="product-button-selections">
+              <div className="product-size-select">
+                <button class="dropbtn size-button">SIZE:</button>
+              </div>
+              <div className="product-color-select">
+                <button class="dropbtn color-button">COLOR: Black/Orange</button>
+              </div>
+            </div>
+            <div className="product-show-attrs">
+              <ul>
+                {
+                  prodAttrs.map(attr => (
+                    <li className="product-show-attr" >{attr.attribute_name}: {attr.attribute_value}</li>
+                  ))
+                }
+              </ul>
+            </div>
+            <div>
+              <button type="submit" className="session-submit">Add to Cart</button>
+            </div>
+          </div>
         </div>
-        <div>
-          <ul>
-            {
-              prodAttrs.map(attr => (
-                <li>{attr.attribute_name}: {attr.attribute_value}</li>
-              ))
-            }
-          </ul>
+
+        <div className="show-index-link">
+          <Link to="/products">Back to products</Link>
         </div>
-        <br/>
-        <Link to="/products">Back to products</Link>
+      
       </div>
     );
   }
