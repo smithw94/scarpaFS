4c641436a93064c3bac3d801b4dc5dc2b2ce5d98
Will Smith <smithw1994@gmail.com>
2019-12-03 13:42:05 -0800

    Sidebar filter update

--- frontend/components/products/product_index/sidebar_filter.jsx
+++ frontend/components/products/product_index/sidebar_filter.jsx
@@ -34,73 +34,67 @@ class SidebarFilter extends React.Component {
           <br/>
 
           <label>              
-            <input type="checkbox" value={5} onChange={this.handleFilterAttrChange}/>
+            <input type="checkbox" value={37} onChange={this.handleFilterAttrChange}/>
             Men's                         
           </label>
           <br/>
 
           <label>
-            <input type="checkbox" value={6} onChange={this.handleFilterAttrChange}/>             
+            <input type="checkbox" value={38} onChange={this.handleFilterAttrChange}/>             
             Women's
           </label>
           <br/>
 
           <label>
-            <input type="checkbox" value={1} onChange={this.handleFilterAttrChange}/>
+            <input type="checkbox" value={33} onChange={this.handleFilterAttrChange}/>
             Approach
           </label>
           <br/>
 
           <label>
-            <input type="checkbox" value={2} onChange={this.handleFilterAttrChange}/>
+            <input type="checkbox" value={34} onChange={this.handleFilterAttrChange}/>
             Bouldering
           </label>
           <br/>
 
           <label>
-            <input type="checkbox" value={3} onChange={this.handleFilterAttrChange}/>
+            <input type="checkbox" value={35} onChange={this.handleFilterAttrChange}/>
             Competition
           </label>
            <br/>
 
           <label>
-            <input type="checkbox" value={4} onChange={this.handleFilterAttrChange} />
+            <input type="checkbox" value={36} onChange={this.handleFilterAttrChange} />
             Trad
           </label>
           <br />
-
-          <label>
-            <input type="checkbox" value={8} onChange={this.handleFilterAttrChange} />
-            Closure
-          </label>
-          <br/>
           
           <label>
-            <input type="checkbox" value={11} onChange={this.handleFilterAttrChange} />
+            <input type="checkbox" value={43} onChange={this.handleFilterAttrChange} />
             Leather
           </label>
           <br/>
           
           <label>
-            <input type="checkbox" value={12} onChange={this.handleFilterAttrChange} />
+            <input type="checkbox" value={44} onChange={this.handleFilterAttrChange} />
             Leather/Synthetic
           </label>
           <br/>
 
           <label>
-            <input type="checkbox" value={13} onChange={this.handleFilterAttrChange} />
+            <input type="checkbox" value={45} onChange={this.handleFilterAttrChange} />
             Synthetic
           </label>
           <br/>
           
           <label>
-            <input type="checkbox" value={15} onChange={this.handleFilterAttrChange} />
+            <input type="checkbox" value={47} onChange={this.handleFilterAttrChange} />
             XS Edge
           </label>
           <br/>
           
           <label>
-            <input type="checkbox" value={16} onChange={this.handleFilterAttrChange} />
+            <input type="checkbox" value={48} onChange={this.handleFilterAttrChange} />
             XS GRIP2
           </label>
 
